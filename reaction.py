298345729f41607e70cc1a9e0903feacb7768a65
Beatrice-peiqilu <3159897866@qq.com>
2026-04-13 19:11:30 +0800

    Add timr

--- reaction.py
+++ reaction.py
@@ -1,5 +1,5 @@
 from gpiozero import LED,Button
-from time import sleep
+from time import sleep,time
 from random import uniform
 import sys
 from signal import pause
@@ -11,11 +11,14 @@ left_button=Button(14)
 left_name=input('left player name is:')
 right_name=input('right player name is:')
 
+start_time=0
+
+reaction_time=time()-start_time
 def pressed(button):
 	if button.pin.number==14:
-		print(left_name+'won the game')
+		print(f"{left_name}pressed in{reaction_time:.3f}seconds and won the game")
 	else:
-		print(right_name+'won the game')
+		print(f"{right_name}pressed in{reaction_time:.3f}seconds and won the game")
 	sys.exit()
 
 right_button.when_pressed=pressed
@@ -25,6 +28,8 @@ print("Get ready...")
 led.on()
 sleep(uniform(5,10))
 led.off()
+
+start_time=time()
 print("GO!")
 
 pause()
