d1d38d5f0384e2d08c8b3049be1c4fdaaa587f68
Beatrice-peiqilu <3159897866@qq.com>
2026-04-13 09:41:13 +0800

    Get players names

--- reaction.py
+++ reaction.py
@@ -1,17 +1,30 @@
 from gpiozero import LED,Button
 from time import sleep
 from random import uniform
+import sys
+from signal import pause
 
 led=LED(4)
 right_button=Button(15)
 left_button=Button(14)
 
+left_name=input('left player name is:')
+right_name=input('right player name is:')
+
 def pressed(button):
-	print(str(button.pin.number)+"won the game")
+	if button.pin.number==14:
+		print(left_name+'won the game')
+	else:
+		print(right_name+'won the game')
+	sys.exit()
 
 right_button.when_pressed=pressed
 left_button.when_pressed=pressed
 
+print("Get ready...")
 led.on()
 sleep(uniform(5,10))
 led.off()
+print("GO!")
+
+pause()
